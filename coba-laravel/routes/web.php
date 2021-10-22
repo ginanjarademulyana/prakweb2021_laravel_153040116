b46cff5cbb5476a50530546cb1a736f086d7b314
ginanjarademulyana <dorion.official@gmail.com>
2021-10-22 15:11:22 +0700

    Menyelesaikan Intruksi pada Video 3 (Struktur Folder, Routes & View)

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Ginanjar Ade Mulyana",
+        "email" => "ginanjaradem@mail.unpas.ac.id",
+        "image" => "Gingin.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
