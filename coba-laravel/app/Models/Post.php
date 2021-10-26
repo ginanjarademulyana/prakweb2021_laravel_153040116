460755ea212ca1a1cb480099cde2461d57e47c72
ginanjarademulyana <dorion.official@gmail.com>
2021-10-26 15:11:15 +0700

    Menyelesaikan intruksi dari Video 7 (Post Model)

--- coba-laravel/app/Models/Post.php
+++ coba-laravel/app/Models/Post.php
@@ -2,40 +2,13 @@
 
 namespace App\Models;
 
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Ginanjar Ade Mulyana",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, assumenda eligendi? At fugiat ab aperiam modi perferendis cupiditate nisi reiciendis non. Dolorem iure fugiat nemo officia similique, unde nesciunt facilis beatae quia reiciendis ratione distinctio quasi autem inventore nisi explicabo rerum voluptas quos, consectetur numquam a voluptates corporis excepturi! Inventore aliquid odio quas, dolore natus optio debitis. Qui obcaecati perferendis minima iusto asperiores, ratione, illum nulla explicabo porro, quis quod expedita earum fugiat dolorum animi totam suscipit aliquid odit sapiente."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Dewi Wisuda Wardani",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, assumenda eligendi? At fugiat ab aperiam modi perferendis cupiditate nisi reiciendis non. Dolorem iure fugiat nemo officia similique, unde nesciunt facilis beatae quia reiciendis ratione distinctio quasi autem inventore nisi explicabo rerum voluptas quos, consectetur numquam a voluptates corporis excepturi! Inventore aliquid odio quas, dolore natus optio debitis. Qui obcaecati perferendis minima iusto asperiores, ratione, illum nulla explicabo porro, quis quod expedita earum fugiat dolorum animi totam suscipit aliquid odit sapiente."
-        ]
-    ];
+    use HasFactory;
 
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-        // $post = [];
-        // foreach ($posts as $p) {
-        //     if ($p['slug'] === $slug) {
-        //         $post = $p;
-        //     }
-        // }
-        return $posts->firstWhere('slug', $slug);
-    }
+    // protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
